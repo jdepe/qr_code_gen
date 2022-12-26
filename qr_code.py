ee9c706e04fceb5f26128dccd3aaf22471c00ab7
jdepe <jaden.dep@gmail.com>
2022-12-26 21:25:16 +1000

    update colour

--- qr_code.py
+++ qr_code.py
@@ -9,7 +9,11 @@ test_2.save("Test2.png", scale = 30, border = 5, dark = "#0041f5", data_dark = "
 
 # Test with details
 test_with_details = helpers.make_mecard(name = "Jaden", email = "jaden.dep@gmail.com", url = "https://www.linkedin.com/in/jaden-de-penning/")
-test_with_details.save("contact.png", scale = 30, border = 5, dark = "#0041f5", data_dark = "yellow", light = "#00dcf5")
+
+test_with_details.designator
+'3-L'
+
+test_with_details.save("contact.png", scale = 5, border = 5, dark = "darkred", data_dark = "darkorange", light = "yellow")
 
 
 
